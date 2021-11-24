3a8b1574a4e9a43b9f0b37fe2dfb5285650d4c10
chagantiramaraju <chaganti.ramaraju@gmail.com>
2021-11-24 21:03:06 +0530

    Stack Object using inbuilt deque from collections module

--- Stack/Stack.py
+++ Stack/Stack.py
@@ -1,8 +1,34 @@
-st = []
-st.append(1)
-st.append(2)
-st.append(3)
+from collections import deque
 
-print(st)
-st.pop()
-print(st)
+class Stack:
+    def __init__(self):
+        self.stack = deque()
+    
+    def push(self, data):
+        self.stack.append(data)
+
+    def pop(self):
+        if len(self.stack) > 0:
+            self.stack.pop()
+    
+    def peek(self):
+        if len(self.stack) > 0:
+            return self.stack[-1]
+        else:
+            return -1
+    
+    def Isempty(self):
+        return len(self.stack) == 0
+
+    def size(self):
+        return len(self.stack)
+
+
+if __name__=='__main__':
+    st = Stack()
+    st.push(5)
+    st.push(10)
+    print(st.stack)
+    st.pop()
+    print(st.stack)
+    print(st.peek())
